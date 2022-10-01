beca21cb7d523d020bfaff7339bd825d0872309f
Soumya Ranjan Choudhury <soumyaranjanchoudhury194@gmail.com>
2022-10-01 20:56:45 +0530

    Update detect_tflite.py

--- detect_tflite.py
+++ detect_tflite.py
@@ -1,12 +1,12 @@
-import tensorflow as tf
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-if len(physical_devices) > 0:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
-    
 # import tensorflow as tf
-# gpus = tf.config.experimental.list_physical_devices('GPU')
-# tf.config.experimental.set_virtual_device_configuration(gpus[0],
-#                        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=2048)])
+# physical_devices = tf.config.experimental.list_physical_devices('GPU')
+# if len(physical_devices) > 0:
+#     tf.config.experimental.set_memory_growth(physical_devices[0], True)
+    
+import tensorflow as tf
+gpus = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_virtual_device_configuration(gpus[0],
+                       [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=2048)])
 
 import cv2
 import time
